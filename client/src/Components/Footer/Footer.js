ce693ebdc825c6312217b1519f59661339b3a6b4
Liz <lizlarkin@highlandtechnology.com>
2021-04-14 16:09:51 -0700

    added login functionality with global userData object, local storage, and history

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -7,41 +7,44 @@ import linkedin from "./Images/linkedin.png"
 const Footer = () => {
     return (
         <footer>
-            <div class = "row navbar navbar-dark bg-dark hti-footer">
-                <div class = "col-md-2">
+            {/* <div className = "row footer-space">
+                <div className = "col-md-12"></div>
+            </div> */}
+            <div className = "row navbar navbar-dark bg-dark hti-footer">
+                <div className = "col-md-2">
                     <Link to="/">
                         <img src={instagram} alt="Instagram"/>
                     </Link>
                 </div>
-                <div class = "col-md-8">
-                    <div class = "row">
-                        <div class = "col-md-12">
-                            <ul class="nav justify-content-center navbar navbar-dark bg-dark">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Careers</a>
+                <div className = "col-md-8">
+                    <div className = "row">
+                        <div className = "col-md-12">
+                            <ul className="nav justify-content-center navbar navbar-dark bg-dark">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Careers</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Customers</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Customers</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">FAQs</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">FAQs</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Partners</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Partners</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Testimonials</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Testimonials</a>
                                 </li>
                             </ul>
                         </div>    
                     </div>
-                    <div class = "row">
-                        <div class = "col-md-12">
+                    <div className = "row">
+                        <div className = "col-md-12">
                             &copy; 2021 Highland Technology, Inc.
                         </div>    
                     </div>
                 </div>
-                <div class = "col-md-2">
+                <div className = "col-md-2">
                     <Link to="/">
                         <img src={linkedin} alt="LinkedIn"/>
                     </Link>
--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import UserContext from "../../Context/UserContext";
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './formStyles.css';
 
 const Login = () => {
 
     const [form, setForm] = useState();
+    const { userData, setUserData } = useContext(UserContext);
+    const history = useHistory();
 
     const onChange = (e) => {
         setForm({...form, [e.target.name]: e.target.value});
@@ -13,33 +18,51 @@ const Login = () => {
     const submitLogin = async (e) => {
         e.preventDefault();
         try {
-            const loginRes = await axios.post("/users/login", form);
-            console.log(loginRes);
+            const { data } = await axios.post("/users/login", form);
+            console.log(data);
+
+            setUserData({
+                token: data.token,
+                user: data.user,
+            })
+
+            localStorage.setItem("auth-token", data.token);
+
+            history.push("/Pages/Home");
+
+    
         } catch (error) {
             console.log(error.response);
         }
     }
 
+    useEffect(() => {
+        console.log(userData);
+    }, [])
+
     return (
         <div>
-            <h1 class = "form-heading">Login</h1>
+            <h1 className = "form-heading">Login</h1>
 
-            <div class ="row">
-                <div class="col-md-4"></div>
-                <div class ="col-md-4 form-box">
+            <div className ="row">
+                <div className="col-md-4"></div>
+                <div className ="col-md-4 form-box">
                     <form onSubmit={submitLogin}>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputEmail1">Email</label>
-                        <input  onChange={onChange} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email"/>
+                        <input  onChange={onChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email"/>
                     </div>
-                    <div class="form-group form-input">
+                    <div className="form-group form-input">
                         <label for="exampleInputPassword1">Password</label>
-                        <input  onChange={onChange} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password"/>
+                        <input  onChange={onChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name="password"/>
                     </div>
-                    <button type="submit" class="btn btn-primary form-input">Submit</button>
+                    <button type="submit" className="btn btn-primary form-input">Submit</button>
+                    <Link to ="/pages/register">
+                        <button type="submit" className="btn btn-primary form-input" id="reg-btn">Register</button>
+                    </Link>
                     </form>
                 </div>
-                <div class="col-md-4"></div>
+                <div className="col-md-4"></div>
             </div>
         </div>
     )
