8cb3df86028ce9e5b7dedff401822943e2be2088
Elizabeth Larkin <lizlarkin@highlandtechnology.com>
2022-12-24 19:39:47 -0800

    added home page carousel

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import "./assets/homeStyles.css";
 import homeLogo from '../../Components/Footer/Images/Highland_logo_white.png';
@@ -7,11 +7,16 @@ import DDG from '../Product/Images/P500_Digital_Delay_Generator_1.png';
 import PulseGen from '../Product/Images/J270_Pulse_Generator_1.png';
 import WaveformGen from '../Product/Images/P350_Waveform_Playback_ARB_1.png';
 import LaserControl from '../Product/Images/D100_Laser_Driver_1.png';
-import Photonics from '../Product/Images/T850_Pockels_Cell_Driver_1.png';
-import MeasureSim from '../Product/Images/T680_Time_Interval_Counter_1.png';
-import Legacy from '../Product/Images/P730_Optical_Electrialc_Fanout_Buffer_1.png';
+import Photonics from '../Product/Images/J724_Electrical_Fiberoptic_Converter_6.png';
+import MeasureSim from '../Product/Images/P545_Synchro_LVDT_Acquisition_Simulator_2.png';
+// import Legacy from '../Product/Images/P730_Optical_Electrialc_Fanout_Buffer_1.png';
 
 const Home = () => {
+
+    // TO DO: 
+    // 1. Jumps on photonics
+    // 2. Fix order (add all?)
+    // 3. Replace background images with own
 
     const history = useHistory();
 
@@ -25,14 +30,14 @@ const Home = () => {
     ]
 
     const products = [
-        ["VME", VME, "VME"],
         ["Digital Delay Generators", DDG, "DDG"],
         ["Laser Drivers & Controllers", LaserControl, "LDC"],
         ["Photonics", Photonics, "PHO"],
         ["Measurement & Simulation", MeasureSim, "MAS"],
+        ["VME", VME, "VME"],
         ["Pulse Generators", PulseGen, "PSG"],
         ["Waveform Generators", WaveformGen, "WFG"],
-        ["Legacy", Legacy, "LEG"],
+        // ["Legacy", Legacy, "LEG"],
     ]
 
     const custom = [
@@ -53,27 +58,46 @@ const Home = () => {
       let [productIndex, setProductIndex] = useState(0);
 
       const incrementProdIndex = () => {
-        if (productIndex === 7) {
+        if (productIndex === 6) {
             setProductIndex(0);
+            updateArrayW();
         } else {
             setProductIndex(idx => idx + 1);
+            updateArrayW();
         }
-        console.log("product index: ", productIndex)
+        updateArrayW();
       }
 
       const decrementProdIndex = () => {
         if (productIndex === 0) {
-            setProductIndex(7)
+            setProductIndex(6)
+            updateArrayW();
         } else {
             setProductIndex(idx => idx - 1)
+            updateArrayW();
         }
         console.log("product index: ", productIndex)
       }
 
-    //   useEffect(() => {
-    //     setProductIndex();
-    //   }, [])
-      
+      let [productArray, setProductArray] = useState([0,1,2,3,4,5,6])
+
+      const updateArrayW = () => {
+        if (productIndex === 0) {
+            setProductArray([1,2,3,4,5,6,0])
+        } else if (productIndex === 1) {
+            setProductArray([2,3,4,5,6,0,1])
+        } else if (productIndex === 2) {
+            setProductArray([3,4,5,6,0,1,2])
+        } else if (productIndex === 3) {
+            setProductArray([4,5,6,0,1,2,3])
+        } else if (productIndex === 4) {
+            setProductArray([5,6,0,1,2,3,4])
+        } else if (productIndex === 5) {
+            setProductArray([6,0,1,2,3,4,5])
+        } else {
+            setProductArray([0,1,2,3,4,5,6])
+        }
+      }
 
     return (
         <div>
@@ -129,16 +153,16 @@ const Home = () => {
                     <div className="row" id="background-product-row">
                         <div className="col-md-3"></div>
                         <div className="col-md-2">
-                            <a className="product-link" href ={"/Category/"}>
-                                <img src={products[productIndex][1]} className="img-thumbnail bgOpacity" alt={products[productIndex][0]}></img>
-                                <h5>{products[productIndex][0]}</h5>
+                            <a className="product-link" href ={"/Category/"+products[productArray[2]][2]}>
+                                <img src={products[productArray[2]][1]} className="img-thumbnail bgOpacity" alt={products[productArray[2]][0]}></img>
+                                <h5>{products[productArray[2]][0]}</h5>
                             </a>
                         </div>
                         <div className="col-md-2"></div>
                         <div className="col-md-2">
-                            <a className="product-link" href ={"/Category/"}>
-                                <img src={products[productIndex][1]} className="img-thumbnail bgOpacity" alt={products[productIndex][0]}></img>
-                                <h5>{products[productIndex][0]}</h5>
+                            <a className="product-link" href ={"/Category/"+products[productArray[3]][2]}>
+                                <img src={products[productArray[3]][1]} className="img-thumbnail bgOpacity" alt={products[productArray[3]][0]}></img>
+                                <h5>{products[productArray[3]][0]}</h5>
                             </a>
                         </div>
                         <div className="col-md-3"></div>
@@ -146,21 +170,21 @@ const Home = () => {
                     <div className="row">
                         <div className="col-md-1"></div>
                         <div className="col-md-3">
-                            <a className="product-link" href ={"/Category/"}>
-                                <img src={products[productIndex][1]} className="img-thumbnail bgOpacity" alt={products[productIndex][0]}></img>
-                                <h5>{products[productIndex][0]}</h5>
+                            <a className="product-link" href ={"/Category/"+products[productArray[1]][2]}>
+                                <img src={products[productArray[1]][1]} className="img-thumbnail bgOpacity" alt={products[productArray[1]][0]}></img>
+                                <h5>{products[productArray[1]][0]}</h5>
                             </a>
                         </div>
                         <div className="col-md-4">
-                            <a className="product-link" href ={"/Category/"}>
-                                <img src={products[productIndex][1]} className="img-thumbnail" id="highlightedProduct" alt={products[productIndex][0]}></img>
-                                <h5>{products[productIndex][0]}</h5>
+                            <a className="product-link" href ={"/Category/"+products[productArray[0]][2]}>
+                                <img src={products[productArray[0]][1]} className="img-thumbnail" id="highlightedProduct" alt={products[productArray[0]][0]}></img>
+                                <h5>{products[productArray[0]][0]}</h5>
                             </a>
                         </div>
                         <div className="col-md-3">
-                            <a className="product-link" href ={"/Category/"}>
-                                <img src={products[productIndex][1]} className="img-thumbnail bgOpacity" alt={products[productIndex][0]}></img>
-                                <h5>{products[productIndex][0]}</h5>
+                            <a className="product-link" href ={"/Category/"+products[productArray[4]][2]}>
+                                <img src={products[productArray[4]][1]} className="img-thumbnail bgOpacity" alt={products[productArray[4]][0]}></img>
+                                <h5>{products[productArray[4]][0]}</h5>
                             </a>
                         </div>
                         <div className="col-md-1"></div>
@@ -168,28 +192,14 @@ const Home = () => {
                     <div className = "row">
                         <div className="col-md-4"></div>
                         <div className="col-md-2">
-                            <button onClick={decrementProdIndex} type="button" className="btn btn-outline-primary carousel-button"><i className="fa-solid fa-chevrons-left"></i></button>
+                            <button onClick={decrementProdIndex} type="button" className="btn btn-outline-light carousel-button"><i className="fa-solid fa-chevrons-left"></i></button>
                         </div>
                         <div className="col-md-2">
-                            <button onClick={incrementProdIndex} type="button" className="btn btn-outline-primary carousel-button"><i className="fa-solid fa-chevrons-right"></i></button>
+                            <button onClick={incrementProdIndex} type="button" className="btn btn-outline-light carousel-button"><i className="fa-solid fa-chevrons-right"></i></button>
                         </div>
                         <div className="col-md-4"></div>
                     </div>
                 </div>
-
-                {/* <div id="product-row">
-                    <h1 className="home-header" >Standard Products</h1>
-                    <div className="row row-cols-8">
-                        {products.map((product, idx) => (
-                            <div className="col" key={idx}>
-                                <a className="product-link" href ={"/Category/"+product[2]}>
-                                    <img src={product[1]} className="img-thumbnail bgOpacity" alt={product}></img>
-                                    <h5>{product[0]}</h5>
-                                </a>
-                            </div>
-                        ))}
-                    </div>
-                </div> */}
 
                 <div className="light-row">
                     <h1 className="home-header">Custom Electronics</h1>
